18341d9a83bff7313a398282f26286c97414f441
Brendan ODowd <bodowd@worcester.edu>
2015-04-05 19:27:39 -0400

    Date struct contents working, person struct contents getting overwritten

--- Driver.c
+++ Driver.c
@@ -9,6 +9,27 @@ const char delim[2] = ","; //the separator for strtok()
 char* token; //to hold the tokens generated from person file
 int i = 0; //loop index
 int temp; //to hold the int found by fscanf()
+int pcount = 0, dcount = 0; //array indexes for person/date arrays
+int num_read = 1; //counter for date reading
+
+//holds date of birth data for PERSONs
+typedef struct {
+	int month;
+	int day;
+	int year;
+} DATE;
+
+typedef struct {
+	char* lname; //last name
+	DATE* dob; //date of birth
+	int ssn;
+	char* address;
+	char* city;
+	int age;
+} PERSON;
+
+DATE dates[17];
+PERSON people[17];
 
 int main() {
 
@@ -34,21 +55,57 @@ int main() {
 	}
 
 	//while we haven't reached the end of the file
-	while(fgets(string,80,per_fp)!=NULL) {
-		token = strtok(string,delim);
-		puts(token);
-
-		//read the rest of the line, there are 4 entries per line after the name
-		for(i = 0; i < 4; i++) {
-			token = strtok(NULL,delim);
-			puts(token);
+	while(fscanf(dob_fp, "%d,", &temp) > 0) {
+		if(num_read % 3 == 1) {
+			dates[dcount].month = temp;	
 		}
+		if(num_read % 3 == 2) {
+			dates[dcount].day = temp;
+		}
+		if(num_read % 3 == 0) {
+			dates[dcount].year = temp;
+			dcount++;
+		}
+		num_read++;
 	}
 
 	//while we haven't reached the end of the file
-	while(fscanf(dob_fp, "%d,", &temp) > 0) {
-		printf("%d\n",temp);
+	while(fgets(string,80,per_fp)!=NULL) {
+		for(i = 0; i < 5; i++) {			
+			if(i == 0) {
+				token = strtok(string,delim);
+				puts(token);
+				people[pcount].lname = token;			
+			}			
+			if(i == 1) {
+				token = strtok(NULL,delim);
+				puts(token);				
+				people[pcount].ssn = atoi(token);
+			}
+			if(i == 2) {
+				token = strtok(NULL,delim);
+				puts(token);
+				people[pcount].address = token;
+			}
+			if(i == 3) {
+				token = strtok(NULL,delim);
+				puts(token);
+				people[pcount].city = token;
+			}
+			if(i == 4) {
+				token = strtok(NULL,delim);
+				puts(token);
+				people[pcount].age = atoi(token);
+			}
+		}
+
+		//people[pcount].dob = *dates[pcount];
+		pcount++; //increment person array index
 	}
 
+	for(i = 0; i < 17; i++) {
+		puts(people[7].lname);
+	}
+	
 	return 0;
 }
